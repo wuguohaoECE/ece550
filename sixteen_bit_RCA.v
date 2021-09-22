274140208780fdc17adbb367b62c4def29c3b530
guohao wu <guohao.wu@duke.edu>
2021-09-22 18:16:48 -0400

    add comma

--- sixteen_bit_RCA.v
+++ sixteen_bit_RCA.v
@@ -9,7 +9,7 @@ module sixteen_bit_RCA(A, B, sum, cin, cout,ovf);
 	output cout,ovf;
 	wire temp;
 	
-	eight_bit_RCA eight_bit_RCA_low(A[BIT-1:0], B[BIT-1:0], sum[BIT-1:0], cin, temp);
+	eight_bit_RCA eight_bit_RCA_low(A[BIT-1:0], B[BIT-1:0], sum[BIT-1:0], cin, temp,);
 	eight_bit_RCA eight_bit_RCA_high(A[SIZE-1:BIT], B[SIZE-1:BIT], sum[SIZE-1:BIT], temp, cout,ovf);
 	
 
